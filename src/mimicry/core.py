3d43d8c9f3f1d5ef4fba43a853fff73ac720c6b8
Kacper Wojtasinski <k0wojtasinski@gmail.com>
2025-06-17 21:12:44 +0200

    chore: fix ruff format issue,

--- src/mimicry/core.py
+++ src/mimicry/core.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import yaml
 
-from mimicry.models import SinkConfiguration, TableConfiguration, FieldConfiguration
+from mimicry.models import SinkConfiguration, TableConfiguration
 
 logger = logging.getLogger(__name__)
 
